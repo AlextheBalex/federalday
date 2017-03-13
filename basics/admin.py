992b932683716ddb6512cb9384193d3c1b0592ff
alexthebalex <voluntarismus@gmail.com>
2017-03-13 14:08:04 +0100

    updated .gitignore, admin.py

--- basics/admin.py
+++ basics/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from basics.models import *
 
+
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ("legislative_period", "document_id", "date")
+
 
 class PartyAdmin(admin.ModelAdmin):
     list_display = ("abbrev", "name")
 
+
 class FunctionAdmin(admin.ModelAdmin):
     list_display = ("name",)
 
+
 class RegularStatementAdmin(admin.ModelAdmin):
     list_display = ("speaker", "text_short_decorator", "document", "order_id", 'str_nouns_short_decorator', 'str_adjectives', 'str_persons')
+
 
 class SpeakerAdmin(admin.ModelAdmin):
     list_display = ('name', "party", "function")
